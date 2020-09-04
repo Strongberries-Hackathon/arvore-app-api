0b2c82466cd2df0d149dc23e92c43e1703e3d247
acole <aniela.cole@move.com>
2020-09-04 12:43:47 -0700

    new api

---
+++ api/book/bookCover.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET book cover. */
+router.get("/", (req, res, next) => {
+  res.send(_fetchCover());
+});
+
+const _fetchCover = () => {
+  return {
+    title: "The Friendly Creeper Diaries (Book 1): The Creeper Village",
+    sutitle: "An Unofficial Minecraft Diary Book for Kids Ages 9 - 12 (Preteen)",
+    author: "Mark Mulle",
+    coverPicture: "/images/bookCover.jpeg",
+  };
+};
+
+module.exports = router;
