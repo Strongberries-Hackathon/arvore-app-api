148c603e355714d9253478af9f2941c82191d169
acole <aniela.cole@move.com>
2020-09-07 11:01:39 -0700

    Updating dataa and adding new endpoint

--- api/routes/grades.js
+++ api/routes/grades.js
@@ -6,4 +6,16 @@ router.get("/", (req, res, next) => {
   res.send(grades);
 });
 
+router.get("/:gradeId", (req, res, next) => {
+  const { gradeId } = req.params;
+  res.send(_fetchGrade(gradeId));
+});
+
+const _fetchGrade = (gradeId) => {
+  return grades.map((grade) => {
+    if (grade.id === gradeId) return grade;
+    else return "Grade not found";
+  });
+};
+
 module.exports = router;
