6a6c9293cace84652fc873244c9f6e352b03ff83
Deehxm <andressa.xarao@outlook.com>
2020-09-07 20:42:21 -0300

    Main Heroku

--- api/index.js
+++ api/index.js
@@ -4,10 +4,14 @@ const router = express.Router();
 /* GET home page. */
 router.get("/", (req, res, next) => {
   res.json({
-    APIs:{/book: {/book/:'bookId'},
-          /grade: {/grade/:'gradeId'},
-          /activity: {/activity/:'activityId',
-                      /activity/upload/:'photo/studant_id'}}
+    APIs: {
+      book: { book: "bookId" },
+      grade: { grade: "gradeId" },
+      activity: {
+        activity: "activityId",
+        "activity/upload": "photo/studant_id",
+      },
+    },
   });
 });
 
